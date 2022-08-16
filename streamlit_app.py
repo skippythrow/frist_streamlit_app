d7eca36ffca4ea42d5108173f39fbf3b70e3a541
skippythrow <hellobobityjoe@gmail.com>
2022-08-16 14:07:21 -0400

    Update streamlit_app.py
    Added pandas and a csv of fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,17 @@
 import streamlit
+import pandas as pd
 
+// Title
 streamlit.title('My Parents New Healthy Diner')
 
+// Breakfast Favorites menu items
 streamlit.header('🥣 Breakfast Favorites')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 
+// Build your own smoothie menu items
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
