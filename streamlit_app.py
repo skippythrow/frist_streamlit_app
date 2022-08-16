b4dc43a912bce4c1ec14f75c9c420ab759cf0eca
skippythrow <hellobobityjoe@gmail.com>
2022-08-16 14:10:52 -0400

    Update streamlit_app.py
    Added selector

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,9 @@ streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 
 # Build your own smoothie menu items
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
